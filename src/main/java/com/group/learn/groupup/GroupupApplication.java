8323634a23aa60bf0861bb98e19886f853b9490f
wangyuancun <christianwyc@foxmail.com>
2023-05-16 13:00:01 +0800

    初始化

---
+++ src/main/java/com/group/learn/groupup/GroupupApplication.java
@@ -0,0 +1,13 @@
+package com.group.learn.groupup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupupApplication.class, args);
+	}
+
+}
